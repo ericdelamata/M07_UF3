28f4a532e1bafd902c1c56b98473a2a89229559b
ericdelamata <2023_eric.de@iticbcn.cat>
2025-02-03 19:23:26 +0100

    pasApas començat

---
+++ TIC_BCN_EMM/pasApas/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('guardar_sesion/', views.guardar_sesion, name='guardar_sesion'),
+    path('recuperar_sesion/', views.recuperar_sesion, name='recuperar_sesion'),
+]
---
+++ TIC_BCN_EMM/pasApas/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def guardar_sesion(request):
+    request.session['usuario'] = 'Juan'
+    return render(request, 'guardar_sesion.html')
+
+
+def recuperar_sesion(request):
+    usuario = request.session.get('usuario', 'Invitado')
+    return render(request, 'recuperar_sesion.html', {'usuario': usuario})

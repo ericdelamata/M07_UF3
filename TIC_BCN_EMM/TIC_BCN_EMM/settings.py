7f36ef1688a49d07d7eb8fcd8626921b9719ecf7
ericdelamata <2023_eric.de@iticbcn.cat>
2025-02-12 20:09:14 +0100

    creacio de l'aplicació

--- TIC_BCN_EMM/TIC_BCN_EMM/settings.py
+++ TIC_BCN_EMM/TIC_BCN_EMM/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'centre',
     'pasApas',
+    'authentication',
 ]
 
 MIDDLEWARE = [
--- TIC_BCN_EMM/authentication/views.py
+++ TIC_BCN_EMM/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Autenticació de l'usuari
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            context['error'] = "Usuari o contrasenya incorrectes."
+    return render(request, 'authentication/login.html', context)
+
+def index(request):
+    context = {
+        'message': 'Benvingut a la pàgina d''inici!'
+    }
+    return render(request, 'authentication/index.html', context)
